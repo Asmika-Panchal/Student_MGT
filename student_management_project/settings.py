84e5cd6ee28bbd1c9a4b8b0f8df0e0fd62773d96
Asmika panchal <123617260+Asmika-Panchal@users.noreply.github.com>
2025-04-22 20:02:17 +0530

    Update settings.py do

--- student_management_project/settings.py
+++ student_management_project/settings.py
@@ -38,7 +38,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'student-mgt-app.azurewebsites.net']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','.vercel.app']
 
 
 
